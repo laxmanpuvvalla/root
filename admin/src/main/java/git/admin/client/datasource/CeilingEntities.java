64386e8e4e3f8b67f3c018ff09a43897677d43e9
laxman <laxman.puvvalla@gmail.com>
2018-09-07 21:30:21 +0530

    all commit

---
+++ admin/src/main/java/git/admin/client/datasource/CeilingEntities.java
@@ -0,0 +1,13 @@
+package git.admin.client.datasource;
+
+
+/**
+ * 
+ * @author jfischer
+ *
+ */
+public class CeilingEntities {
+	
+	public static final String SHIPPING_COUNTRY = "git.core.catalog.domain.ShippingCountry";
+	
+}
---
+++ admin/src/main/java/git/admin/client/datasource/EntityImplementations.java
@@ -0,0 +1,13 @@
+package git.admin.client.datasource;
+
+
+/**
+ * 
+ * @author jfischer
+ *
+ */
+public class EntityImplementations {
+	
+	public static final String PRODUCT = "git.core.catalog.domain.MyCompanyProductImpl";
+
+}
